20cbae4b73cdd344cb73c9da7025f037100f1245
Yuxin Ye <yuxin_ye@protonmail.com>
2023-07-01 13:11:17 +1000

    add deckrouter and individual deck page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import {useEffect, useState} from "react";
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 type TDeck = {
-  _id: string,
-  title: string,
-}
+  _id: string;
+  title: string;
+};
 
 function App() {
   const [decks, setDecks] = useState<TDeck[]>([]);
@@ -15,11 +16,11 @@ function App() {
     const response = await fetch("http://localhost:3000/decks", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({title}),
-    })
-    const deck = await response.json() as TDeck;
+      body: JSON.stringify({ title }),
+    });
+    const deck = (await response.json()) as TDeck;
     setDecks([...decks, deck]);
     setTitle("");
   }
@@ -27,7 +28,7 @@ function App() {
   async function handleDeleteDeck(id: string) {
     await fetch(`http://localhost:3000/decks/${id}`, {
       method: "DELETE",
-    })
+    });
     // optimistic update
     const newDecks = decks.filter((deck: TDeck) => deck._id !== id);
     setDecks(newDecks);
@@ -36,34 +37,35 @@ function App() {
   useEffect(() => {
     async function fetchDecks() {
       const response = await fetch("http://localhost:3000/decks");
-      const newDecks = await response.json() as TDeck[];
+      const newDecks = (await response.json()) as TDeck[];
       setDecks(newDecks);
-    } 
+    }
     fetchDecks();
-  }, [])
+  }, []);
 
-  return <div className="App">
-    <div className="decks">
-      {decks.map((deck: TDeck) => (
+  return (
+    <div className="App">
+      <div className="decks">
+        {decks.map((deck: TDeck) => (
           <li key={deck._id}>
-            {deck.title}
-            <button onClick={()=> handleDeleteDeck(deck._id)}>X</button>
+            <Link to={`/decks/${deck._id}`}>{deck.title}</Link>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
           </li>
-        ))
-      }
+        ))}
+      </div>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Create Deck</button>
+      </form>
     </div>
-    <form onSubmit={handleCreateDeck}>
-      <label htmlFor="deck-title">Deck Title</label>
-      <input 
-        id="deck-title"
-        value={title}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>  {
-          setTitle(e.target.value)
-        }}
-      />
-      <button>Create Deck</button>
-    </form>
-  </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Deck from "./Deck.tsx";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/decks/:deckId",
+    element: <Deck />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
